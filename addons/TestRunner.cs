7af188e7882607a08b754f10823d4c90306a545c
semickolon <mickolenulud@gmail.com>
2020-10-20 00:00:05 +0800

    Refactor TestRunner

--- addons/TestRunner.cs
+++ addons/TestRunner.cs
@@ -12,10 +12,13 @@ namespace NUG
   public class TestRunner
   {
     private readonly SceneTree _sceneTree;
-    private readonly Dictionary<Type, List<TestCase>> _testCases = new Dictionary<Type, List<TestCase>>();
-    private readonly Dictionary<Type, List<MethodInfo>> _setupMethods = new Dictionary<Type, List<MethodInfo>>();
-    private readonly Dictionary<Type, List<MethodInfo>> _teardownMethods = new Dictionary<Type, List<MethodInfo>>();
-
+    private readonly Dictionary<TestContext, List<TestCase>> _testCases = new Dictionary<TestContext, List<TestCase>>();
+    
+    private readonly MethodRegistry _setupMethods = new MethodRegistry();
+    private readonly MethodRegistry _teardownMethods = new MethodRegistry(true);
+    private readonly MethodRegistry _oneTimeSetupMethods = new MethodRegistry();
+    private readonly MethodRegistry _oneTimeTeardownMethods = new MethodRegistry(true);
+    
     public TestRunner(SceneTree sceneTree)
     {
       _sceneTree = sceneTree;
@@ -24,21 +27,26 @@ namespace NUG
 
     public async Task Run(Action<TestResult> callback)
     {
-      foreach (var type in _testCases.Keys)
+      foreach (var context in _testCases.Keys)
       {
-        var testCases = _testCases[type]!;
+        var testCases = _testCases[context]!;
+
+        _oneTimeSetupMethods.InvokeAll(context);
+        
         foreach (var testCase in testCases)
         {
-          var testResult = await RunTestCase(testCase);
+          var testResult = await RunTestCase(context, testCase);
           callback(testResult);
         }
+
+        _oneTimeTeardownMethods.InvokeAll(context);
       }
     }
 
-    private async Task<TestResult> RunTestCase(TestCase testCase)
+    private async Task<TestResult> RunTestCase(TestContext context, TestCase testCase)
     {
+      var testObject = context.CreateTestObject();
       var method = testCase.Method;
-      var testObject = testCase.TestObject;
       var testResult = new TestResult(method);
       
       try
@@ -48,8 +56,7 @@ namespace NUG
           _sceneTree.Root.AddChild(node);
         }
 
-        _setupMethods.TryGetValue(method.DeclaringType, out var setupMethods);
-        setupMethods?.ForEach(x => x.Invoke(testObject, new object[] { }));
+        _setupMethods.InvokeAll(context, testObject);
 
         object obj = method.Invoke(testObject, testCase.Parameters);
 
@@ -69,8 +76,7 @@ namespace NUG
       }
       finally
       {
-        _teardownMethods.TryGetValue(method.DeclaringType, out var teardownMethods);
-        teardownMethods?.ForEach(x => x.Invoke(testObject, new object[] { }));
+        _teardownMethods.InvokeAll(context, testObject);
 
         (testObject as Node)?.QueueFree();
       }
@@ -105,14 +111,13 @@ namespace NUG
           if (constructor == null)
             continue; // TODO really?
 
-          var methods = concreteType.GetMethods();
           var testCases = new List<TestCase>();
-          
           object CreateTestObject() => constructor.Invoke(testFixtureAttr.Arguments);
+          var context = new TestContext(concreteType, CreateTestObject);
           
-          foreach (var method in methods)
+          foreach (var method in concreteType.GetMethods())
           {
-            var testCase = ScanMethod(concreteType, method, CreateTestObject);
+            var testCase = ScanMethod(context, method);
             testCase?.With(x => testCases.Add(x));
           }
 
@@ -120,36 +125,41 @@ namespace NUG
             continue;
           
           testCases.Sort((x, y) => x.Order - y.Order);
-          _testCases[concreteType] = testCases;
-          
-          _setupMethods.TryGetValue(concreteType)?.Sort(new MethodHierarchyComparer());
-          _teardownMethods.TryGetValue(concreteType)?.Sort(new MethodHierarchyComparer(true));
+          _testCases[context] = testCases;
         }
       }
     }
 
-    private TestCase? ScanMethod(Type type, MethodInfo method, Func<object> createTestObject)
+    private TestCase? ScanMethod(TestContext context, MethodInfo method)
     {
-      var testCaseAttrs = GetCustomAttributes<TestCaseAttribute>(method).ToList();
-      var orderAttr = GetCustomAttribute<OrderAttribute>(method);
-      var setupAttr = GetCustomAttribute<SetUpAttribute>(method);
-      var teardownAttr = GetCustomAttribute<TearDownAttribute>(method);
-
-      if (setupAttr != null)
+      if (HasCustomAttribute<SetUpAttribute>(method))
       {
-        _setupMethods.ValueOrNew(type).Add(method);
+        _setupMethods.AddMethod(context, method);
       }
-      else if (teardownAttr != null)
+      else if (HasCustomAttribute<TearDownAttribute>(method))
       {
-        _teardownMethods.ValueOrNew(type).Add(method);
+        _teardownMethods.AddMethod(context, method);
+      }
+      else if (HasCustomAttribute<OneTimeSetUpAttribute>(method))
+      {
+        _oneTimeSetupMethods.AddMethod(context, method);
+      }
+      else if (HasCustomAttribute<OneTimeTearDownAttribute>(method))
+      {
+        _oneTimeTeardownMethods.AddMethod(context, method);
       }
       else
       {
+        var testCaseAttrs = GetCustomAttributes<TestCaseAttribute>(method).ToList();
+        
         if (testCaseAttrs.Count == 0)
         {
           var testAttr = GetCustomAttribute<TestAttribute>(method);
-          var testCaseAttr = testAttr?.ToTestCaseAttribute();
-          testCaseAttr?.With(x => testCaseAttrs.Add(x));
+          if (testAttr != null)
+          {
+            var testCaseAttr = ToTestCaseAttribute(testAttr);
+            testCaseAttrs.Add(testCaseAttr);
+          }
         }
         
         foreach (var testCaseAttr in testCaseAttrs)
@@ -157,11 +167,11 @@ namespace NUG
           if (testCaseAttr == null || testCaseAttr.Ignore != null || testCaseAttr.IgnoreReason != null)
             return null;
 
+          var orderAttr = GetCustomAttribute<OrderAttribute>(method);
           var order = orderAttr?.Order ?? int.MaxValue;
-          var testPayload = new TestCase(method, createTestObject(), 
-            testCaseAttr.Arguments, testCaseAttr.ExpectedResult, order);
           
-          return testPayload;
+          var testCase = new TestCase(method, testCaseAttr.Arguments, testCaseAttr.ExpectedResult, order);
+          return testCase;
         }
       }
 
@@ -186,6 +196,11 @@ namespace NUG
 
       return null;
     }
+
+    private static bool HasCustomAttribute<T>(MemberInfo type) where T : Attribute
+    {
+      return Attribute.IsDefined(type, typeof(T));
+    }
     
     private static T? GetCustomAttribute<T>(MemberInfo type) where T : Attribute
     {
@@ -195,6 +210,16 @@ namespace NUG
     private static T[] GetCustomAttributes<T>(MemberInfo type) where T : Attribute
     {
       return (T[]) Attribute.GetCustomAttributes(type, typeof(T));
+    }
+    
+    private static TestCaseAttribute ToTestCaseAttribute(TestAttribute testAttr)
+    {
+      var testCaseAttr = new TestCaseAttribute();
+      testAttr.Author?.With(x => testCaseAttr.Author = x);
+      testAttr.Description?.With(x => testCaseAttr.Description = x);
+      testAttr.ExpectedResult?.With(x => testCaseAttr.ExpectedResult = x);
+      testAttr.TestOf?.With(x => testCaseAttr.TestOf = x);
+      return testCaseAttr;
     }
   }
 
@@ -213,34 +238,31 @@ namespace NUG
     }
   }
 
+  public readonly struct TestContext
+  {
+    public readonly Type Type;
+    public readonly Func<object> CreateTestObject;
+
+    public TestContext(Type type, Func<object> createTestObject)
+    {
+      Type = type;
+      CreateTestObject = createTestObject;
+    }
+  }
+
   public readonly struct TestCase
   {
     public readonly MethodInfo Method;
-    public readonly object TestObject;
     public readonly object[] Parameters;
     public readonly object? ExpectedResult;
     public readonly int Order;
 
-    public TestCase(MethodInfo method, object testObject, object[] parameters, object? expectedResult, int order)
+    public TestCase(MethodInfo method, object[] parameters, object? expectedResult, int order)
     {
       Method = method;
-      TestObject = testObject;
       Parameters = parameters;
       ExpectedResult = expectedResult;
       Order = order;
-    }
-  }
-
-  public static class NUnitExtensions
-  {
-    public static TestCaseAttribute ToTestCaseAttribute(this TestAttribute testAttr)
-    {
-      var testCaseAttr = new TestCaseAttribute();
-      testAttr.Author?.With(x => testCaseAttr.Author = x);
-      testAttr.Description?.With(x => testCaseAttr.Description = x);
-      testAttr.ExpectedResult?.With(x => testCaseAttr.ExpectedResult = x);
-      testAttr.TestOf?.With(x => testCaseAttr.TestOf = x);
-      return testCaseAttr;
     }
   }
 
@@ -250,35 +272,68 @@ namespace NUG
     {
       action(t);
     }
-
-    public static TValue ValueOrNew<TKey, TValue>(this Dictionary<TKey, TValue> dict, TKey key)
-      where TValue : new()
-    {
-      if (!dict.ContainsKey(key))
-      {
-        dict[key] = new TValue();
-      }
-
-      return dict[key];
-    }
-
-    public static TValue? TryGetValue<TKey, TValue>(this Dictionary<TKey, TValue> dict, TKey key)
-      where TValue : class
-    {
-      return dict.ContainsKey(key) ? dict[key] : null;
-    }
   }
 
-  public class MethodHierarchyComparer : IComparer<MethodInfo>
+  public class MethodRegistry
   {
-    private readonly bool _reverse;
+    private readonly Dictionary<TestContext, List<MethodInfo>> _store = new Dictionary<TestContext, List<MethodInfo>>();
+    private readonly HashSet<TestContext> _dirtySet = new HashSet<TestContext>();
+    private readonly bool _reverseCallOrder;
 
-    public MethodHierarchyComparer(bool reverse = false)
+    public MethodRegistry(bool reverseCallOrder = false)
     {
-      _reverse = reverse;
+      _reverseCallOrder = reverseCallOrder;
+    }
+    
+    public void InvokeAll(TestContext context, object? testObject = null)
+    {
+      GetMethods(context)?.ForEach(m =>
+      {
+        m.Invoke(testObject ?? context.CreateTestObject(), new object[] {});
+      });
     }
 
-    public int Compare(MethodInfo x, MethodInfo y)
+    public void AddMethod(TestContext context, MethodInfo method)
+    {
+      if (!_store.ContainsKey(context))
+      {
+        _store[context] = new List<MethodInfo>();
+      }
+      
+      _store[context].Add(method);
+      SetDirty(context);
+    }
+
+    private List<MethodInfo>? GetMethods(TestContext context)
+    {
+      if (!_store.ContainsKey(context))
+        return null;
+
+      if (IsDirty(context))
+      {
+        SortFor(context);
+      }
+
+      return _store[context];
+    }
+    
+    private bool IsDirty(TestContext context)
+    {
+      return _dirtySet.Contains(context);
+    }
+
+    private void SetDirty(TestContext context)
+    {
+      _dirtySet.Add(context);
+    }
+
+    private void SortFor(TestContext context)
+    {
+      _store[context].Sort(CompareByHierarchy);
+      _dirtySet.Remove(context);
+    }
+    
+    private int CompareByHierarchy(MethodInfo x, MethodInfo y)
     {
       var xt = x.DeclaringType!;
       var yt = y.DeclaringType!;
@@ -287,7 +342,7 @@ namespace NUG
         return 0;
 
       var c = xt.IsSubclassOf(yt) ? 1 : -1;
-      c *= _reverse ? -1 : 1;
+      c *= _reverseCallOrder ? -1 : 1;
       return c;
     }
   }
